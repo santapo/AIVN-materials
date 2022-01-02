3c21aeaaee9725fd0d7f3c421703ad6364589eaa
santapo <nguyentrongtuank62hust@gmail.com>
2022-01-02 15:39:45 +0000

    first training

--- image_analysis/train.sh
+++ image_analysis/train.sh
@@ -3,5 +3,6 @@ python cli.py \
     --trainer.max_epochs=200 \
     --model.optimizer="sgd" \
     --model.learning_rate=0.1 \
+    --data.data_dir="/root/splitted_flowers" \
     --data.batch_size=128
     # --lr_scheduler="torch.optim.lr_scheduler.CosineAnnealingLR"
