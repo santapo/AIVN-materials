e9b9c8c54f4e9356de9f37353ca6aa4d4c061fc5
santapo <nguyentrongtuank62hust@gmail.com>
2022-09-19 15:12:04 +0700

    force polygon to standing rectangle

--- object_tracking/dataset.py
+++ object_tracking/dataset.py
@@ -11,7 +11,7 @@ class VOT14Reader:
         self.all_samples_path = glob.glob(os.path.join(self.dataset_path, "*/"))
 
     def __len__(self):
-        return len(self.all_dataset_path)
+        return len(self.all_samples_path)
 
     def __getitem__(self, idx):
         all_images_path = glob.glob(os.path.join(self.all_samples_path[idx], "images", "*"))
@@ -35,10 +35,13 @@ class VOT14Reader:
         all_tlwh_gt = []
         for polygon in groundtruth:
             polygon = [float(pts) for pts in polygon.split(",")]
-            polygon = np.array(polygon, dtype=np.int32).reshape(-1, 1, 2)
-            all_tlwh_gt.append([polygon[0][0][0], polygon[1][0][1],
-                                polygon[2][0][0] - polygon[0][0][0],
-                                polygon[3][0][1] - polygon[1][0][1]])
+            polygon = np.array(polygon, dtype=np.int32).reshape(4, 2)
+
+            x1 = np.clip(np.min(polygon[:, 0]), 0, None)
+            y1 = np.clip(np.min(polygon[:, 1]), 0, None)
+            x2 = np.max(polygon[:, 0])
+            y2 = np.max(polygon[:, 1])
+            all_tlwh_gt.append([x1, y1, x2-x1, y2-y1])
 
         return all_images, all_tlwh_gt, all_images_name
 
