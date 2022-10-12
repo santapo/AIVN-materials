d9cbfb0e6c39f6595353059e5833892cf74baf04
santapo <nguyentrongtuank62hust@gmail.com>
2022-10-12 11:36:47 +0700

    minor revamp

--- object_tracking/scratch/camshift.py
+++ object_tracking/scratch/camshift.py
@@ -4,25 +4,25 @@ from typing import List
 import numpy as np
 
 
-def camshift(probImage: np.ndarray[np.int8], window: List[int]) -> List[int]:
+def camshift(prob_image: np.ndarray[np.int8], window: List[int]) -> List[int]:
     x, y, w, h = window
 
     M00, M01, M10 = 0, 0, 0
     for i in range(x, x + w):
         for j in range(y, y + h):
-            M00 = M00 + probImage[i, j]
-            M10 = M10 + probImage[i, j] * j
-            M01 = M01 + probImage[i, j] * i
+            M00 = M00 + prob_image[i, j]
+            M10 = M10 + prob_image[i, j] * j
+            M01 = M01 + prob_image[i, j] * i
 
     xc = M01 / M00
-    if xc + w/2 > probImage.shape[0]:
-        xc = probImage.shape[0] - w/2
+    if xc + w/2 > prob_image.shape[0]:
+        xc = prob_image.shape[0] - w/2
     yc = M10 / M00
-    if yc + h/2 > probImage.shape[1]:
-        yc = probImage.shape[1] - h/2
+    if yc + h/2 > prob_image.shape[1]:
+        yc = prob_image.shape[1] - h/2
     
     new_x = math.floor(xc) - w/2
     new_y = math.floor(yc) - h/2
     new_w = math.floor(2 * math.sqrt(M00/256))
     new_h = 1.2 * new_w
-    return new_x, new_y, new_w, new_h
+    return None, [new_x, new_y, new_w, new_h]
--- object_tracking/scratch/meanshift.py
+++ object_tracking/scratch/meanshift.py
@@ -4,22 +4,23 @@ from typing import List
 import numpy as np
 
 
-def meanshift(probImage: np.ndarray[np.int8], window: List[int]) -> List[int]:
+def meanshift(prob_image: np.ndarray[np.int8], window: List[int]) -> List[int]:
     x, y, w, h = window
 
     M00, M01, M10 = 0, 0, 0
     for i in range(x, x + w):
         for j in range(y, y + h):
-            M00 = M00 + probImage[i, j]
-            M10 = M10 + probImage[i, j] * j
-            M01 = M01 + probImage[i, j] * i
+            M00 = M00 + prob_image[i, j]
+            M10 = M10 + prob_image[i, j] * j
+            M01 = M01 + prob_image[i, j] * i
 
     xc = M01 / M00
-    if xc + w/2 > probImage.shape[0]:
-        xc = probImage.shape[0] - w/2
+    if xc + w/2 > prob_image.shape[0]:
+        xc = prob_image.shape[0] - w/2
     yc = M10 / M00
-    if yc + h/2 > probImage.shape[1]:
-        yc = probImage.shape[1] - h/2
+    if yc + h/2 > prob_image.shape[1]:
+        yc = prob_image.shape[1] - h/2
 
-    new_window = [math.floor(xc) - w/2, math.floor(yc) - h/2, w, h]
-    return new_window
+    new_x = math.floor(xc) - w/2
+    new_y = math.floor(yc) - h/2
+    return None, [new_x, new_y, w, h]
