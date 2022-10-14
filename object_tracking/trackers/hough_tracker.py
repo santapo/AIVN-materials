f17b747bbeb9bb1fafaaccc49e6b936ade7dbc04
santapo <nguyentrongtuank62hust@gmail.com>
2022-10-14 16:34:32 +0700

    refactor: rename hog to hough

--- object_tracking/trackers/hough_tracker.py
+++ object_tracking/trackers/hough_tracker.py
@@ -10,7 +10,7 @@ from .base_tracker import BaseTracker
 logger = logging.getLogger()
 
 
-class HOGTracker(BaseTracker):
+class HOUGHTracker(BaseTracker):
     def __init__(self, image: np.ndarray, roi_window: List[int], tracker: Callable):
         super().__init__(image, roi_window, tracker)
         logger.info(f"Init {self.__class__.__name__} Successfully!")
@@ -29,10 +29,10 @@ class HOGTracker(BaseTracker):
 
     def get_probability_map(self, image: np.ndarray) -> np.ndarray:
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv],[0], self.roi_hist,[0,180],1)
+        dst = cv2.calcBackProject([hsv], [0], self.roi_hist, [0,180], 1)
         x, y, w, h = self.get_current_window()
-        m_dst = utils.f_dst_weights(image, y,x,h,w)
-        tmp = dst*m_dst
+        m_dst = utils.f_dst_weights(image, y, x, h, w)
+        tmp = dst * m_dst
         tmp = tmp.astype('uint8')
         image_g = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
